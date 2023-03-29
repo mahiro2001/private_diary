b452425a6e3a23c15f69d83bf45e5945afec7b61
mahiro2001 <mahimahiro05@gmail.com>
2023-03-29 12:02:02 +0900

    アラートによる送信可否の表示

--- diary/views.py
+++ diary/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from .forms import InquiryForm
 import logging
 from django.urls import reverse_lazy
+from django.contrib import messages
 # Create your views here.
 
 logger = logging.getLogger(__name__)
@@ -17,6 +18,7 @@ class InquiryView(generic.FormView):
 
     def form_valid(self, form):
         form.send_email()
+        messages.success(self.request, 'メッセージを送信しました')
         logger.info('Inquiry sent by()'.format(form.cleaned_data['name']))
         return super().form_valid(form)
     
